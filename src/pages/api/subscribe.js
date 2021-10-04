38440392bf410744f92497ec6952b97b0f96cd9d
Tiago <tiago.americano.03@gmail.com>
2021-10-04 11:50:02 -0300

    add .env

---
+++ src/pages/api/subscribe.js
@@ -0,0 +1,13 @@
+import connectToDatabase from '../../config/mongodb';
+
+export default async (req, res) => {
+  const { db, client } = await connectToDatabase(process.env.MONGODB_URI);
+  if (client.isConnected()) {
+    const collection = db.collection('subscribers');
+
+    await collection.insertOne({ ...req.body, subscriberAt: new Date() });
+
+    return res.status('200').json({ eai: true });
+  }
+  return res.status('500').json({ error: 'client DB is not connected' });
+};
